c5774dfb798da05265cb50c84ea7ecd851a69299
Ashley Ellis <anellis12@gmail.com>
2016-03-22 14:45:57 -0400

    Save dummy active state in local storage

--- toggle-dummy.js
+++ toggle-dummy.js
@@ -1,8 +1,33 @@
-  $(function(){
-    var button =
-      $('<button type="button" onclick="toggleDummy()"> Show dummies</button>');
-    $("body").prepend(button);
-  });
-  function toggleDummy(){
-    $(".dummy").toggleClass("is-active")
+$(function(){
+  addButton()
+
+  if (exposingDummyElements()){
+    toggleActiveElement();
   }
+});
+
+function addButton() {
+  var button = $('<button type="button" onclick="toggleDummyElements()">Show dummies</button>');
+  $("body").prepend(button);
+}
+
+function toggleDummyElements(){
+  toggleActiveElement();
+  toggleStorage();
+}
+
+function toggleActiveElement() {
+  $(".dummy").toggleClass("is-active");
+}
+
+function toggleStorage() {
+  if (exposingDummyElements()) {
+    localStorage.setItem('showDummy', "false");
+  } else {
+    localStorage.setItem('showDummy', "true");
+  }
+}
+
+function exposingDummyElements() {
+  return localStorage.showDummy == "true"
+}
